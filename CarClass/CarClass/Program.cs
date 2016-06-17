7ec1617ed546fab22e632963591d1ab07b369173
eldher <eldher02@gmail.com>
2016-06-16 21:48:05 -0500

    Clase con Instancia

---
+++ CarClass/CarClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CarClass
+{
+    class Car
+    {
+        private static int CarCounter = 0;
+        private string plates = null;
+
+        public Car(string plate)
+        {
+            this.plates = plate;
+            CarCounter++;                        
+        }
+        public static int GetCarsCount()
+        {
+            return Car.CarCounter;
+        }
+        public string GetPlate()
+        {
+            return this.plates;
+        }    
+
+    }
+
+    class UseCar
+    {
+        static void Main()
+        {
+            Console.WriteLine("Inserte la placa del vehiculo");
+            Car[] lote = new Car[10];
+            for (int i = 0; Car.GetCarsCount()<5; i++)
+            {
+                //string plate = Console.ReadLine();
+                lote[i] = new Car(Console.ReadLine());
+            }
+        }            
+    }
+}
---
+++ CarClass/CarClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CarClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("CarClass")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b70cc7f3-7011-4be6-ba7c-0babfdff0566")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
